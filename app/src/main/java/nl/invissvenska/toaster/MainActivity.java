5d3b21bf164175917cce1ebdacd55e565347f05e
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-06-06 14:54:16 +0200

    Added more different configurations to sample app

--- app/src/main/java/nl/invissvenska/toaster/MainActivity.java
+++ app/src/main/java/nl/invissvenska/toaster/MainActivity.java
@@ -16,45 +16,73 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Toaster.Config.getInstance()
-//                .setGravity(Gravity.START, 950,0)
-                .setMargin(40f, 0f)
-                .apply();
-
         findViewById(R.id.buttonNormal).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Toaster.normal(getApplicationContext(), "Toast Normal", Toaster.LENGTH_LONG).show();
-            //    Toaster.Config.reset();
+                Toaster.normal(getApplicationContext(), R.string.normal_toast).show();
+            }
+        });
+        findViewById(R.id.buttonIconNormal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toaster.normal(getApplicationContext(), R.string.normal_icon_toast, getResources().getDrawable(R.drawable.ic_shipping_24, null)).show();
             }
         });
 
         findViewById(R.id.buttonWarning).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toaster.warning(getApplicationContext(), "Toast Warning", Toaster.LENGTH_LONG).show();
+                Toaster.warning(getApplicationContext(), R.string.warning_toast).show();
             }
         });
 
         findViewById(R.id.buttonInfo).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toaster.info(getApplicationContext(), "Toast Info", Toaster.LENGTH_LONG).show();
+                Toaster.info(getApplicationContext(), R.string.info_toast, Toaster.LENGTH_LONG).show();
             }
         });
 
         findViewById(R.id.buttonSuccess).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toaster.success(getApplicationContext(), "Toast Success", Toaster.LENGTH_LONG).show();
+                Toaster.success(getApplicationContext(), R.string.success_toast, Toaster.LENGTH_LONG).show();
             }
         });
 
         findViewById(R.id.buttonError).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toaster.error(getApplicationContext(), "Toast Error", Toaster.LENGTH_LONG).show();
+                Toaster.error(getApplicationContext(), R.string.error_toast, Toaster.LENGTH_LONG).show();
+            }
+        });
+
+        findViewById(R.id.buttonCustom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toaster.custom(getApplicationContext(), R.string.custom_toast, getResources().getDrawable(R.drawable.ic_cafe_24, null), R.color.colorToastCustom, Toaster.LENGTH_LONG, true, true).show();
+            }
+        });
+
+        findViewById(R.id.buttonGravity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toaster.Config.getInstance()
+                        .setGravity(Gravity.TOP, 0, 50)
+                        .apply();
+                Toaster.normal(getApplicationContext(), R.string.gravity_toast).show();
+                Toaster.Config.reset();
+            }
+        });
+
+        findViewById(R.id.buttonMargin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toaster.Config.getInstance()
+                        .setMargin(0f, 0.1f)
+                        .apply();
+                Toaster.custom(getApplicationContext(), R.string.margin_toast, null, R.color.colorToastCustom2, Toaster.LENGTH_LONG, false, true).show();
+                Toaster.Config.reset();
             }
         });
     }
