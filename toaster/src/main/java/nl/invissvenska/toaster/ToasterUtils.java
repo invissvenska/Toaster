a4f454f2b1c80958afdfcae6c334769e319816a8
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-06-06 11:10:29 +0200

    wip

---
+++ toaster/src/main/java/nl/invissvenska/toaster/ToasterUtils.java
@@ -0,0 +1,46 @@
+package nl.invissvenska.toaster;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.os.Build;
+import android.view.View;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
+
+public class ToasterUtils {
+
+    private ToasterUtils() {
+    }
+
+    static Drawable tintIcon(@NonNull Drawable drawable, @ColorInt int tintColor) {
+        drawable.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN);
+        return drawable;
+    }
+
+    static Drawable tint9PatchDrawableFrame(@NonNull Context context, @ColorInt int tintColor) {
+        final NinePatchDrawable toastDrawable = (NinePatchDrawable) getDrawable(context, R.drawable.toast_frame);
+        return tintIcon(toastDrawable, tintColor);
+    }
+
+    static void setBackground(@NonNull View view, Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            view.setBackground(drawable);
+        else
+            view.setBackgroundDrawable(drawable);
+    }
+
+    static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) {
+        return AppCompatResources.getDrawable(context, id);
+    }
+
+    static int getColor(@NonNull Context context, @ColorRes int color){
+        return ContextCompat.getColor(context, color);
+    }
+}
